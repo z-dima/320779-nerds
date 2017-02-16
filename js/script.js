2e93eff76d64db4895692c9cfb607474afb22e1d
z-dima <zarechnev.dima.92@mail.ru>
2017-02-16 17:05:52 +1000

    Доработка проекта
    Удалены лишние элементы и комментарии.
    Добавлен минимизированный файл css.
    Создан общий спрайт.

--- js/script.js
+++ js/script.js
@@ -1,43 +1,34 @@
 var link = document.querySelector(".feedback-open");
-
 var popup = document.querySelector(".feedback");
 var close = popup.querySelector(".btn-close");
-
 var form = popup.querySelector(".feedback-form");
 var username = popup.querySelector("[name=username]");
 var email = popup.querySelector("[name=e-mail]");
-
 var storage = localStorage.getItem("login");
 
 link.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.add("feedback-show");
-
     if (storage) {
         username.value = storage;
         password.focus();
     } else {
         username.focus();
     }
-
 }); //Открытие формы
-
 close.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.remove("feedback-show");
 }); //Закрытие формы
-
 form.addEventListener("submit", function(event) {
     if (!username.value || !email.value) {
         event.preventDefault();
         popup.classList.remove("feedback-error");
         popup.offsetWidth = popup.offsetWidth;
         popup.classList.add("feedback-error");
-
         username.classList.remove("info-error");
         username.offsetWidth = popup.offsetWidth;
         username.classList.add("info-error");
-
         email.classList.remove("info-error");
         email.offsetWidth = popup.offsetWidth;
         email.classList.add("info-error");
@@ -45,7 +36,6 @@ form.addEventListener("submit", function(event) {
         localStorage.setItem("username", login.value);
     }
 }); //Изменение события отправки формы
-
 window.addEventListener("keydown", function(event) {
     if (event.keyCode === 27) {
         if (popup.classList.contains("feedback-show")) {
@@ -63,7 +53,6 @@ function init(){
         center: [59.938414, 30.323072],
         zoom: 17
     }); 
-
     myPlacemark = new ymaps.Placemark([59.93870788, 30.32301386], { 
         hintContent: 'NERDS', 
         balloonContent: 'NERDS' 
@@ -73,6 +62,5 @@ function init(){
         iconImageOffset: [-185, -165],
         iconImageSize: [367, 190] 
     }); 
-
     myMap.geoObjects.add(myPlacemark); 
-    }
+}
